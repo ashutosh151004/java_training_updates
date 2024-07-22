f489289079374d3c1a139412fb387feffbbc5efc
ashutosh151004 <ashutosh151004@gmail.com>
2024-07-22 11:13:06 +0530

    first commit

---
+++ test/src/Length.java
@@ -0,0 +1,10 @@
+class Length {
+    public static void main(String[] args) {
+        int len=0;
+        String s = "preparation";
+        for(char cl : s.toCharArray())
+        len++;
+        System.out.println("Length of String is:" +len);
+
+    }
+}
