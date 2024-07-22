a3ee9c1bf878aaf35e56cf0d95b80c3559e74963
ashutosh151004 <ashutosh151004@gmail.com>
2024-07-22 15:19:56 +0530

    new Code

---
+++ test/src/Convert.java
@@ -0,0 +1,9 @@
+public class Convert{
+    public static void main(String[] Args)
+    {
+        int a = 5;
+        float b = 7.5f;
+        float sum = a+b;
+        System.out.println("The sum is:" +sum);
+    }
+} 
