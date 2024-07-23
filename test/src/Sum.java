f0369e1e8d275d205707374dc8e096d5d309752e
ashutosh151004 <ashutosh151004@gmail.com>
2024-07-23 15:32:53 +0530

    Code Added

---
+++ test/src/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+    public static void main(String[] args) {
+        int i=1,sum=0;
+        {
+            while(i<=10){
+                sum =sum+i;
+                i++;
+            }
+            System.out.println("Sum of natural no upto 10 is:" +sum);
+        }
+    }
+    
+    
+}
